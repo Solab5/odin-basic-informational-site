a1d758e9137309933048cbbf90cd2b00b06bccf2
morris <morristwinomugisha2000@gmail.com>
2025-01-03 21:40:08 +0300

    fix: use express to link web pages

--- index.js
+++ index.js
@@ -1,40 +1,45 @@
 const http = require('http');
+const express = require('express');
 const path = require('path');
-const fs = require('fs')
+const fs = require('fs');
 
-const server = http.createServer((req, res) => {
-    if (req.url === '/') {
+
+const app = express();
+
+app.get("/", (req, res) => {
         fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) => {
             if (err) throw err;
             res.writeHead(200, {'content-Type': 'text/html'})
             res.end(content)
         });
     }
+);
 
-    else if (req.url === '/about') {
-        fs.readFile(path.join(__dirname, 'public', 'about.html'), (err, content) => {
-            if (err) throw err;
-            res.writeHead(200, {'content-Type': 'text/html'})
-            res.end(content)
-        });
-    }
+app.get('/about', (req, res) => {
+    fs.readFile(path.join(__dirname, 'public', 'about.html'), (err, content) => {
+        if (err) throw err;
+        res.writeHead(200, {'content-Type': 'text/html'})
+        res.end(content)
+    });
+}) 
 
-    else if (req.url === '/contact-me') {
-        fs.readFile(path.join(__dirname, 'public', 'contact-me.html'), (err, content) => {
-            if (err) throw err;
-            res.writeHead(200, {'content-Type': 'text/html'})
-            res.end(content)
-        });
-    }
-    else {
-        fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content) => {
-            if (err) throw err;
-            res.writeHead(200, {'content-Type': 'text/html'})
-            res.end(content)
-        });
-    }
+app.get('/contact-me', (req, res) => {
+    fs.readFile(path.join(__dirname, 'public', 'contact-me.html'), (err, content) => {
+        if (err) throw err;
+        res.writeHead(200, {'content-Type': 'text/html'})
+        res.end(content)
+    });
 });
 
-const PORT = process.env.port || 8080
+app.get('*', (req, res) => {
+    fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content) => {
+        if (err) throw err;
+        res.writeHead(200, {'content-Type': 'text/html'})
+        res.end(content)
+    });
+}) 
 
-server.listen(PORT, () => console.log(`Server running on ${PORT}`));
+
+const PORT = process.env.port || 3000
+
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
